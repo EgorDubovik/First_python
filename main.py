79a39e6ec6331a7ea69f591884abd44a54a8a452
EgorDubovik <posik.web.m@gmail.com>
2021-10-25 18:39:36 -0500

    Resize image and save

--- main.py
+++ main.py
@@ -3,13 +3,26 @@ import cv2 as cv
 import img64
 import base64
 
+imagesPath = 'images'
+
 def base64_to_cvImage():
     original = base64.b64decode(img64.img64)
     npimg = np.frombuffer(original, dtype=np.uint8)
     cv_image = cv.imdecode(npimg, 1)
-    cv.imshow('test', cv_image)
+    resize(cv_image, 200)
     #cv.imwrite('test.jpg',cv_image)
 
-base64_to_cvImage()
+def resize(image,scale):
+    width, height = image.shape[:2]
+    dim = (int(height*scale/100), int(width*scale/100))
+    image_resize = cv.resize(image, dim);
+    return image_resize
+
+def save_image(image, imageName):
+    cv.imwrite(imagesPath+'/'+imageName)
+
+image = base64_to_cvImage()
+cv.imshow('Resize image', image)
+
 cv.waitKey(0)
 cv.destroyAllWindows()
