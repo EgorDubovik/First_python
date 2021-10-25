5fd73135752050046c5522176cd6f8e95ae079b3
EgorDubovik <posik.web.m@gmail.com>
2021-10-24 20:34:40 -0500

    base64_to_cvImage

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-import numpy as nm
+import numpy as np
 import cv2 as cv
 import img64
+import base64
 
-def viewImage():
-    print(img64.img64[:10])
+def base64_to_cvImage():
+    original = base64.b64decode(img64.img64)
+    npimg = np.frombuffer(original, dtype=np.uint8)
+    cv_image = cv.imdecode(npimg, 1)
+    cv.imshow('test', cv_image)
+    #cv.imwrite('test.jpg',cv_image)
 
-viewImage()
+base64_to_cvImage()
+cv.waitKey(0)
+cv.destroyAllWindows()
