49c4e70b660fdc4fb27af80fec1eeb66f2fc490e
EgorDubovik <posik.web.m@gmail.com>
2021-10-24 20:21:31 -0500

    Load image base64String

--- main.py
+++ main.py
@@ -1,21 +1,8 @@
 import numpy as nm
+import cv2 as cv
+import img64
 
-def print_hi(name):
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def viewImage():
+    print(img64.img64[:10])
 
-
-def multiply(a, b):
-    return nm.dot(a, b)
-
-
-if __name__ == '__main__':
-    print_hi('Yahor')
-    b = nm.array([1, 2, 3])
-    a = nm.array([
-        [1, 2, 3],
-        [4, 5, 2]
-    ])
-    c = multiply(a, b)
-    print(c)
-
-
+viewImage()
